8b91ac48f3566376841dbb05c5df8a0d025caef7
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-12-14 02:36:24 +0900

    Support :duct.profile/test and :test environment
    - Refactor module test cases

--- test/duct/module/pedestal_test.clj
+++ test/duct/module/pedestal_test.clj
@@ -6,63 +6,90 @@
 (duct/load-hierarchy)
 
 (t/deftest module-test
-  (t/testing "environment: production"
-    (t/testing "no options"
-      (let [config {:duct.profile/base
-                    {:duct.core/project-ns 'some-api}
-                    :duct.profile/prod {}
-                    :duct.module/pedestal {}}]
-        (t/is (= {:duct.core/project-ns 'some-api
-                  :duct.core/environment :production
-                  :duct.server/pedestal
-                  {:base-service sut/prod-service
-                   :default? true
-                   :dev? false}}
-                 (duct/prep-config config)))))
-    (t/testing "options specified"
-      (let [service-map #:io.pedestal.http{:routes #{}
-                                           :port 8888}
-            config {:duct.profile/base
-                    {:duct.core/project-ns 'some-api
-                     :duct.server/pedestal {:service service-map}}
-                    :duct.profile/prod {}
-                    :duct.module/pedestal {:default? false
-                                           :dev? true}}]
-        (t/is (= {:duct.core/project-ns 'some-api
-                  :duct.core/environment :production
-                  :duct.server/pedestal
-                  {:service service-map
-                   :base-service sut/prod-service
-                   :default? false
-                   :dev? true}}
-                 (duct/prep-config config))))))
-  (t/testing "environment: development"
-    (t/testing "no options"
-      (let [config {:duct.profile/base
-                    {:duct.core/project-ns 'some-api}
-                    :duct.profile/dev {}
-                    :duct.module/pedestal {}}]
-        (t/is (= {:duct.core/project-ns 'some-api
-                  :duct.core/environment :development
-                  :duct.server/pedestal
-                  {:base-service sut/dev-service
-                   :default? true
-                   :dev? true}}
-                 (duct/prep-config config [:duct.profile/dev])))))
-    (t/testing "options specified"
-      (let [service-map #:io.pedestal.http{:routes #{}
-                                           :port 8888}
-            config {:duct.profile/base
-                    {:duct.core/project-ns 'some-api
-                     :duct.server/pedestal {:service service-map}}
-                    :duct.profile/dev {}
-                    :duct.module/pedestal {:default? false
-                                           :dev? false}}]
-        (t/is (= {:duct.core/project-ns 'some-api
-                  :duct.core/environment :development
-                  :duct.server/pedestal
-                  {:service service-map
-                   :base-service sut/dev-service
-                   :default? false
-                   :dev? false}}
-                 (duct/prep-config config)))))))
+  (let [base-config {:duct.profile/base
+                     {:duct.core/project-ns 'some-api}
+                     :duct.profile/prod {}
+                     :duct.profile/dev {}
+                     :duct.profile/test {}
+                     :duct.module/pedestal {}}]
+    (t/testing "environment: production"
+      (t/testing "no options"
+        (let [config base-config]
+          (t/is (= {:duct.core/project-ns 'some-api
+                    :duct.core/environment :production
+                    :duct.server/pedestal
+                    {:base-service sut/prod-service
+                     :default? true
+                     :dev? false}}
+                   (duct/prep-config config [:duct.profile/prod])))))
+      (t/testing "options specified"
+        (let [service-map #:io.pedestal.http{:routes #{}
+                                             :port 8888}
+              config (duct/merge-configs
+                      base-config
+                      {:duct.profile/base
+                       {:duct.server/pedestal {:service service-map}}
+                       :duct.module/pedestal {:default? false
+                                              :dev? true}})]
+          (t/is (= {:duct.core/project-ns 'some-api
+                    :duct.core/environment :production
+                    :duct.server/pedestal
+                    {:service service-map
+                     :base-service sut/prod-service
+                     :default? false
+                     :dev? true}}
+                   (duct/prep-config config [:duct.profile/prod]))))))
+    (t/testing "environment: development"
+      (t/testing "no options"
+        (let [config base-config]
+          (t/is (= {:duct.core/project-ns 'some-api
+                    :duct.core/environment :development
+                    :duct.server/pedestal
+                    {:base-service sut/dev-service
+                     :default? true
+                     :dev? true}}
+                   (duct/prep-config config [:duct.profile/dev])))))
+      (t/testing "options specified"
+        (let [service-map #:io.pedestal.http{:routes #{}
+                                             :port 8888}
+              config (duct/merge-configs
+                      base-config
+                      {:duct.profile/base
+                       {:duct.server/pedestal {:service service-map}}
+                       :duct.module/pedestal {:default? false
+                                              :dev? false}})]
+          (t/is (= {:duct.core/project-ns 'some-api
+                    :duct.core/environment :development
+                    :duct.server/pedestal
+                    {:service service-map
+                     :base-service sut/dev-service
+                     :default? false
+                     :dev? false}}
+                   (duct/prep-config config [:duct.profile/dev]))))))
+    (t/testing "environment: test"
+      (t/testing "no options"
+        (let [config base-config]
+          (t/is (= {:duct.core/project-ns 'some-api
+                    :duct.core/environment :test
+                    :duct.server/pedestal
+                    {:base-service sut/dev-service
+                     :default? true
+                     :dev? true}}
+                   (duct/prep-config config [:duct.profile/test])))))
+      (t/testing "options specified"
+        (let [service-map #:io.pedestal.http{:routes #{}
+                                             :port 8888}
+              config (duct/merge-configs
+                      base-config
+                      {:duct.profile/base
+                       {:duct.server/pedestal {:service service-map}}
+                       :duct.module/pedestal {:default? false
+                                              :dev? false}})]
+          (t/is (= {:duct.core/project-ns 'some-api
+                    :duct.core/environment :test
+                    :duct.server/pedestal
+                    {:service service-map
+                     :base-service sut/dev-service
+                     :default? false
+                     :dev? false}}
+                   (duct/prep-config config [:duct.profile/test]))))))))
