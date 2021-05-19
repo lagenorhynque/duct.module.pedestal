5f8dd9324633ffc3d2586deba1bedb4d1082356d
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2021-05-19 20:43:38 +0900

    Update dependencies

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
                  [duct/core "0.8.0"]
 
                  ;; Pedestal
-                 [io.pedestal/pedestal.service "0.5.8"]
+                 [io.pedestal/pedestal.service "0.5.9"]
 
                  ;; Pedestal server adapter
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.5.8"]
-                 ;; [io.pedestal/pedestal.immutant "0.5.8"]
-                 ;; [io.pedestal/pedestal.tomcat "0.5.8"]
+                 [io.pedestal/pedestal.jetty "0.5.9"]
+                 ;; [io.pedestal/pedestal.immutant "0.5.9"]
+                 ;; [io.pedestal/pedestal.tomcat "0.5.9"]
 
                  ;; logging
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
@@ -23,10 +23,10 @@
                  [org.slf4j/jul-to-slf4j "1.7.30"]
                  [org.slf4j/log4j-over-slf4j "1.7.30"]]
   :deploy-repositories [["releases" :clojars]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
-                                  [pjstadig/humane-test-output "0.10.0"]]
-                   :plugins [[jonase/eastwood "0.3.11"]
-                             [lein-cloverage "1.1.2"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.3"]
+                                  [pjstadig/humane-test-output "0.11.0"]]
+                   :plugins [[jonase/eastwood "0.4.2"]
+                             [lein-cloverage "1.2.2"]
                              [lein-kibit "0.1.8"]]
                    :aliases {"test-coverage" ^{:doc "Execute cloverage."}
                              ["with-profile" "test" "cloverage" "--codecov" "--junit"]
