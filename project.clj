afb851ab92b994a5656af3b69461ad17b9560732
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2021-12-15 18:31:15 +0900

    Version 2.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct.module.pedestal "2.1.4-SNAPSHOT"
+(defproject duct.module.pedestal "2.1.4"
   :description "Duct module for Pedestal"
   :url "https://github.com/lagenorhynque/duct.module.pedestal"
   :license {:name "Eclipse Public License"
